2f8c0f25d5417137720e1ff64a3fd8c29283cd19
yaryar2 <yaroslav.karaninsky@gmail.com>
2019-04-03 11:25:32 +0500

    test

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -24,7 +24,7 @@ class PageController extends Controller
 		return response()->json($page);
     }
 	public function add_link(Request $request) {
-        return response()->json($request->input('link_address'));
+        return response()->json($request->post('link_address'));
 		$user = $request->user();
         $page = Page::where('user_id', $user->id)->first();
         $image = '';
